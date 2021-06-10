bc1320d921a78450b42430d2d39e3f462008938b
shantel.kemunto@student.moringaschool.com <kemuntoshantel0@gmail.com>
2021-06-10 23:02:36 +0300

    Add click here button to display day of the week

--- java.js
+++ java.js
@@ -1,3 +1,10 @@
-function birthday(dateOfBirth){
-    return dayOfTheWeek
+// function to get day of the week born
+var DOB = "January 25, 2002";
+function dayOfTheWeek(birthday){
+    d = parseInt(((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD) %7
+    console.log(d);
 }
+
+dayOfTheWeek();
+
+
