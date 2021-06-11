607973e31f3fd3167ee68bda2e2adc4061405f0c
shantel.kemunto@student.moringaschool.com <kemuntoshantel0@gmail.com>
2021-06-11 19:15:10 +0300

    Make changes to bring an alert if the user puts a wrong month

---
+++ script.js
@@ -0,0 +1,52 @@
+function giveAkanName(){
+
+    event.preventDefault();
+
+    
+
+  var dayOfTheWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  var akanMale = ["Kwasi","Kwado","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+  var akanFemale = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+ 
+  var month = parseInt(document.getElementById("month").value);
+  var year = parseInt(document.getElementById("year").value);
+  var day = parseInt(document.getElementById("day").value);
+  var dateOfBirth = new Date(year + "/" + month + "/" + day);
+  document.getElementById("para").innerHTML = dateOfBirth
+
+  var results = dateOfBirth.getDay();
+  var output = document.getElementById("output");
+  var male = document.getElementById("male")
+  var female = document.getElementById("female")
+
+ if (month =="" && year =="" && day =="") {
+     alert("PLease Fill The Form")
+     return false;
+}
+ if (year < 0) {
+     output.style.background ="blue"
+     output.style.color= "white"
+     output.innerHTML = " please enter a valid year! "
+}
+
+    
+ else if ((month < 1) || (month > 31)) {
+     output.style.background ="blue"
+     output.style.background ="white"
+     output.innerHTML = "please enter a valid month!"
+ }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
