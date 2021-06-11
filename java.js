312272cd30ea044dbf5483920b2223273cdc18a3
shantel.kemunto@student.moringaschool.com <kemuntoshantel0@gmail.com>
2021-06-11 16:10:36 +0300

    Made changes to alert the user if they leave the fields blank

--- java.js
+++ java.js
@@ -1,7 +1,30 @@
-var CC, YY, MM, DD, d, 
 var dayOfTheWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 var akanMale = ["Kwasi","Kwado","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 var akanFemale = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+ 
+var month = parseInt(document.getElementById("month").nodeValue);
+var year = parseInt(document.getElementById("year").nodeValue);
+var day = parseInt(document.getElementById("day").nodeValue);
+var dateOfBirth = new Date(year + "/" + month + "/" + day);
+var results = dateOfBirth.getDay();
+var output = document.getElementById("output");
+var male = document.getElementById("male")
+var female = document.getElementById("female")
+
+if (month =="" && year =="" && day =="") {
+    alert("PLease Fill The Form")
+    return false;
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
